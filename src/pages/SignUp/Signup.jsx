7661ad1f2c145e961e1777af07f52ac0e1eba99c
Adel-abualrub <adelbassam5201@gmail.com>
2026-02-08 15:12:17 +0200

    Register Commit

--- src/pages/SignUp/Signup.jsx
+++ src/pages/SignUp/Signup.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box, Button, TextField, Typography, Alert } from '@mui/material'; // إضافة Alert
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
 export default function Signup() {
+  const [Error, SetError] = useState(false);
+
+  const { register, handleSubmit } = useForm({});
+  
+  const SendData = async (values) => {
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_BURL}/auth/Account/Register`, values);
+      console.log(response);
+    } catch (error) {
+      SetError(true);
+      console.log(error, "error");
+    }
+  }
+
   return (
-    <div>
-      signUp page
-    </div>
-  )
+    <Box sx={{ display: 'flex', gap: 3, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
+      <Typography component="h1" variant='h3'>Create your account </Typography>
+
+      {/* عرض الخطأ إذا كان موجود */}
+      {Error && <Alert severity="error">حدث خطأ أثناء التسجيل. يرجى المحاولة مرة أخرى.</Alert>}
+
+      <form onSubmit={handleSubmit(SendData)} sx={{ display: 'flex', flexDirection: 'column', gap: 3, alignItems: 'center' }}>
+        <TextField {...register('email')} id="outlined-basic" label="Email" variant="outlined" sx={{ width: 600 }} />
+        <TextField {...register('userName')} id="outlined-basic" label="UserName" variant="outlined" sx={{ width: 600 }} />
+        <TextField {...register('fuuName')} id="outlined-basic" label="Full Name" variant="outlined" sx={{ width: 600 }} />
+        <TextField {...register('password')} id="outlined-password-input" label="Password" type="password" autoComplete="current-password" sx={{ width: 600 }} />
+        <TextField {...register('phoneNumber')} id="outlined-basic" label="Phone Number" variant="outlined" type="tel" sx={{ width: 600 }} />
+
+        <Button variant="contained" type='submit'>Register</Button>
+      </form>
+    </Box>
+  );
 }
