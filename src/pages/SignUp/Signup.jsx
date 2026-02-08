8f81edcaecc0ea602c5bec5a9a866ef9d6d58dd5
Adel-abualrub <adelbassam5201@gmail.com>
2026-02-08 19:58:09 +0200

    Client side validation

--- src/pages/SignUp/Signup.jsx
+++ src/pages/SignUp/Signup.jsx
@@ -2,12 +2,45 @@ import React, { useState } from 'react';
 import { Box, Button, TextField, Typography, Alert } from '@mui/material'; // إضافة Alert
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
-
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup';
 export default function Signup() {
-  const [Error, SetError] = useState(false);
 
-  const { register, handleSubmit } = useForm({});
-  
+const ValidationSchema = yup.object({
+  userName: yup
+    .string()
+    .required('User Name is required')
+    .min(3, 'User Name must be at least 3 characters'),
+
+  fullName: yup
+    .string()
+    .required('Full Name is required')
+    .min(3, 'Full Name must be at least 3 characters'),
+
+  phoneNumber: yup
+    .string()
+    .required('Phone Number is required')
+    .matches(/^[0-9]{10}$/, 'Phone number must be exactly 10 digits'),
+
+  email: yup
+    .string()
+    .required('Email is required')
+    .email('Invalid email format'),
+
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(6, 'Password must be at least 6 characters'),
+});
+
+
+
+  const [Error, SetError] = useState(false);
+  const { register, handleSubmit,formState :{errors} } = useForm({
+resolver: yupResolver(ValidationSchema)
+
+
+  });
   const SendData = async (values) => {
     try {
       const response = await axios.post(`${import.meta.env.VITE_BURL}auth/Account/Register`, values);
@@ -25,11 +58,11 @@ export default function Signup() {
       
 
       <Box component="form" onSubmit={handleSubmit(SendData)} sx={{ display: 'flex', flexDirection: 'column', gap: 3, alignItems: 'center' }}>
-        <TextField {...register('email')} id="outlined-basic" label="Email" variant="outlined" sx={{ width: 600 }} />
-        <TextField {...register('userName')} id="outlined-basic" label="UserName" variant="outlined" sx={{ width: 600 }} />
-        <TextField {...register('fuuName')} id="outlined-basic" label="Full Name" variant="outlined" sx={{ width: 600 }} />
-        <TextField {...register('password')} id="outlined-password-input" label="Password" type="password" autoComplete="current-password" sx={{ width: 600 }} />
-        <TextField {...register('phoneNumber')} id="outlined-basic" label="Phone Number" variant="outlined" type="tel" sx={{ width: 600 }} />
+        <TextField {...register('email')}  errors={!!errors.email} helperText={errors.email?.message} id="outlined-basic" label="Email" variant="outlined" sx={{ width: 600 }} />
+        <TextField {...register('userName')}  errors={errors.userName} helperText={errors.userName?.message} id="outlined-basic" label="UserName" variant="outlined" sx={{ width: 600 }} />
+        <TextField {...register('fullName')} errors={errors.fullName} helperText={errors.fullName?.message} id="outlined-basic" label="Full Name" variant="outlined" sx={{ width: 600 }} />
+        <TextField {...register('password')} errors={errors.password} helperText={errors.password?.message}  id="outlined-password-input" label="Password" type="password" autoComplete="current-password" sx={{ width: 600 }} />
+        <TextField {...register('phoneNumber')} errors={errors.phoneNumber} helperText={errors.phoneNumber?.message} id="outlined-basic" label="Phone Number" variant="outlined" type="tel" sx={{ width: 600 }} />
 
         <Button variant="contained" type='submit'>Register</Button>
       </Box>
