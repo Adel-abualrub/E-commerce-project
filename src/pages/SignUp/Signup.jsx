971831063320259a55b6319c8468eb388d738ae2
Adel-abualrub <adelbassam5201@gmail.com>
2026-02-09 23:30:38 +0200

    Fix simple error in validation

--- src/pages/SignUp/Signup.jsx
+++ src/pages/SignUp/Signup.jsx
@@ -58,11 +58,11 @@ resolver: yupResolver(ValidationSchema)
       
 
       <Box component="form" onSubmit={handleSubmit(SendData)} sx={{ display: 'flex', flexDirection: 'column', gap: 3, alignItems: 'center' }}>
-        <TextField {...register('email')}  errors={!!errors.email} helperText={errors.email?.message} id="outlined-basic" label="Email" variant="outlined" sx={{ width: 600 }} />
-        <TextField {...register('userName')}  errors={errors.userName} helperText={errors.userName?.message} id="outlined-basic" label="UserName" variant="outlined" sx={{ width: 600 }} />
-        <TextField {...register('fullName')} errors={errors.fullName} helperText={errors.fullName?.message} id="outlined-basic" label="Full Name" variant="outlined" sx={{ width: 600 }} />
-        <TextField {...register('password')} errors={errors.password} helperText={errors.password?.message}  id="outlined-password-input" label="Password" type="password" autoComplete="current-password" sx={{ width: 600 }} />
-        <TextField {...register('phoneNumber')} errors={errors.phoneNumber} helperText={errors.phoneNumber?.message} id="outlined-basic" label="Phone Number" variant="outlined" type="tel" sx={{ width: 600 }} />
+        <TextField {...register('email')}  error={!!errors.email} helperText={errors.email?.message} id="outlined-basic" label="Email" variant="outlined" sx={{ width: 600 }} />
+        <TextField {...register('userName')}  error={!!errors.userName} helperText={errors.userName?.message} id="outlined-basic" label="UserName" variant="outlined" sx={{ width: 600 }} />
+        <TextField {...register('fullName')} error={!!errors.fullName} helperText={errors.fullName?.message} id="outlined-basic" label="Full Name" variant="outlined" sx={{ width: 600 }} />
+        <TextField {...register('password')} error={!!errors.password} helperText={errors.password?.message}  id="outlined-password-input" label="Password" type="password" autoComplete="current-password" sx={{ width: 600 }} />
+        <TextField {...register('phoneNumber')} error={!!errors.phoneNumber} helperText={errors.phoneNumber?.message} id="outlined-basic" label="Phone Number" variant="outlined" type="tel" sx={{ width: 600 }} />
 
         <Button variant="contained" type='submit'>Register</Button>
       </Box>
